e4fccfa9d0dd2b4c93678056b61c16d2604d57ab
sung-ilkim <skim88.1942@gmail.com>
2021-03-28 17:16:26 +0900

    Test code with jsonPath

---
+++ src/main/java/com/skim88/myproject/springboot/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.skim88.myproject.springboot.web;
+
+import com.skim88.myproject.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        String hello = "hello";
+        return hello;
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
