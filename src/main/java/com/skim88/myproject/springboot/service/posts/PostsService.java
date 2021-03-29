a86e4a474247beba622977d7b1849812470af68c
sung-ilkim <skim88.1942@gmail.com>
2021-03-29 22:28:25 +0900

    build ApiController and test codes

---
+++ src/main/java/com/skim88/myproject/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.skim88.myproject.springboot.service.posts;
+
+import com.skim88.myproject.springboot.domain.posts.PostsRepository;
+import com.skim88.myproject.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
