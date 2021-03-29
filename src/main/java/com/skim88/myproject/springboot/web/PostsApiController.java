d1cee3df9a6fa44657ca22eedd9547026f916e1e
sung-ilkim <skim88.1942@gmail.com>
2021-03-29 23:00:41 +0900

    build JPA example and test codes

---
+++ src/main/java/com/skim88/myproject/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.skim88.myproject.springboot.web;
+
+import com.skim88.myproject.springboot.service.posts.PostsService;
+import com.skim88.myproject.springboot.web.dto.PostsResponseDto;
+import com.skim88.myproject.springboot.web.dto.PostsSaveRequestDto;
+import com.skim88.myproject.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
--- src/test/java/com/skim88/myproject/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/skim88/myproject/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.skim88.myproject.springboot.web;
 import com.skim88.myproject.springboot.domain.posts.Posts;
 import com.skim88.myproject.springboot.domain.posts.PostsRepository;
 import com.skim88.myproject.springboot.web.dto.PostsSaveRequestDto;
+import com.skim88.myproject.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,6 +28,7 @@ public class PostsApiControllerTest {
     @LocalServerPort
     private int port;
 
+    // JPA 기능까지 한번에 테스트 하기 위해서 사용 (@WebMvcTest는 JPA 등을 제외한 외부 연동과 관련된 부분만 활성화됨)
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -62,5 +66,41 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
     }
 }
