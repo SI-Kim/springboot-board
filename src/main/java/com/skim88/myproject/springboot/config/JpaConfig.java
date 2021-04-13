2b5cf8a2328a1cc2e8a790d9470c3f8678bd5fca
sung-ilkim <skim88.1942@gmail.com>
2021-04-13 23:17:59 +0900

    fix sources for test

---
+++ src/main/java/com/skim88/myproject/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.skim88.myproject.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
